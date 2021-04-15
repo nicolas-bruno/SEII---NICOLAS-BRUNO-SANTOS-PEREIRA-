af84d30492cc44c31a52988359d80fd8c8b58f40
nicolas_bruno <nicolasbrunop@gmail.com>
2021-04-15 11:27:32 -0300

    semana6

---
+++ Semana06/Calculadora/calculadora.js
@@ -0,0 +1,16 @@
+var valor;
+var resultado;
+
+function botao(num) {
+    valor = document.calc.visor.value += num;
+}
+
+function reseta() {
+    document.calc.visor.value = '';
+}
+
+function calcula() {
+    resultado = eval(valor);
+    document.calc.visor.value = resultado.toLocaleString('pt-br');
+
+}
